b1d45e1f007fe47a2fe2e97364289628f2f3ac41
kmudb <47783830+kmudb@users.noreply.github.com>
2022-04-04 07:59:36 -0600

    archivo principal
    lee los archivos de conexion y functions

---
+++ index.php
@@ -0,0 +1,63 @@
+﻿<?php
+include('lib/functions.php');
+header('content-type: application/json');
+
+
+if($_SERVER['REQUEST_METHOD']=="GET")
+{
+  if(isset($_GET['usuario'])){
+  $usu = $_GET['usuario'];
+  $pwd = $_GET['pwd'];
+  $json = get_usuario($usu,$pwd);
+  if(empty($json))
+  header("HTTP/1.1 404 Not Found");
+  echo json_encode($json);
+  }
+  elseif(isset($_GET['id']))
+  {
+    $id =  $_GET['id'];
+    $json = get_un_equipo_info($id);
+    if(empty($json))
+    header("HTTP/1.1 404 Not Found");
+    echo json_encode($json);
+  }
+  else{
+    $json = get_all_equipos_lista();
+    echo json_encode($json);
+  }
+}
+
+
+
+if($_SERVER['REQUEST_METHOD']=="POST")
+{
+  $data = json_decode( file_get_contents( 'php://input') );
+  
+  $id = $data->Id;
+  $token = $data->Token;
+  $datos=json_encode($data->Datos);
+   $json = add_equipo_info($id,$token,$datos);
+  echo json_encode($json);
+}
+
+if($_SERVER['REQUEST_METHOD']=="PUT")
+{
+  $data = json_decode( file_get_contents( 'php://input' ) );
+  
+  $id = $data->Id;
+  $token = $data->Token;
+  $datos=json_encode($data->Datos);
+  
+  $json = update_equipo_info($id,$token,$datos);
+  echo json_encode($json);
+}
+
+if($_SERVER['REQUEST_METHOD']=="DELETE")
+{
+  $data = json_decode( file_get_contents( 'php://input' ));
+  
+  $id = $data->Id;
+  $json = delete_equipo_info($id);
+  echo json_encode($json);
+}
+?>
